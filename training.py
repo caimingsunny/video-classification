cd13fc38be307a2016e5b71da863168388d5feab
xuyu0010 <xuyu0014@e.ntu.edu.sg>
2023-05-02 15:26:49 +0800

    outputs = model(inputs, proposals)

--- training.py
+++ training.py
@@ -80,8 +80,8 @@ def train_epoch(epoch,
             proposals = proposals.view(-1, T//det_interval, nrois, 4)
             if len(inputs) < max_N:
                 proposals = proposals[:len(inputs)]
-            proposals_d = proposals.detach()
-            outputs = model(inputs, proposals_d)
+            # proposals_d = proposals.detach()
+            outputs = model(inputs, proposals)
             # update to the largest batch_size
             max_N = max(N, max_N)
         else:
